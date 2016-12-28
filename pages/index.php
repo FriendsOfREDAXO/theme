1eb920087caf324e4509619266f7562762fbac9c
DanielWeitenauer <github@studio-ahoi.de>
2016-12-28 21:04:32 +0100

    Add info subpage

--- pages/index.php
+++ pages/index.php
@@ -4,9 +4,9 @@
  *
  * @var rex_addon $this
  */
-echo rex_view::title(rex_i18n::msg('name'));
 
-if($subpage = rex_be_controller::getCurrentPagePart(2))
-{
+echo rex_view::title($this->i18n('name'));
+
+if ($subpage = rex_be_controller::getCurrentPagePart(2)) {
     include rex_be_controller::getCurrentPageObject()->getSubPath();
 }
