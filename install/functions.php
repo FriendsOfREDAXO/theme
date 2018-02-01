8fdcd4affa1e6b2c46a8fb60544820029e8b058a
Daniel Weitenauer <github@studio-ahoi.de>
2018-02-01 20:21:35 +0100

    CS

--- install/functions.php
+++ install/functions.php
@@ -1,17 +1,17 @@
 <?php
 rex_extension::register('PACKAGES_INCLUDED', function () {
-    // Register custom yrewrite scheme
-    //if (rex_addon::get('yrewrite')->isAvailable()) {
-    //    rex_yrewrite::setScheme(new rex_theme_rewrite_scheme());
-    //}
+    // Register custom YRewrite scheme
+    /*if (rex_addon::get('yrewrite')->isAvailable()) {
+        rex_yrewrite::setScheme(new my_rewrite_scheme());
+    }*/
 
     // Register YForm templates
-    //if (rex_addon::get('yform')->isAvailable()) {
-    //    rex_yform::addTemplatePath(theme_path::base().DIRECTORY_SEPARATOR.'private'.DIRECTORY_SEPARATOR.'ytemplates');
-    //}
+    /*if (rex_addon::get('yform')->isAvailable()) {
+        rex_yform::addTemplatePath(theme_path::base('private/ytemplates');
+    }*/
 
     // Register fragment folder
-    //rex_fragment::addDirectory(theme_path::base().DIRECTORY_SEPARATOR.'private'.DIRECTORY_SEPARATOR.'fragments');
+    //rex_fragment::addDirectory(theme_path::base('private/fragments');
 }, 'LATE');
 
 
