af37943e3fd86c1eefdbb7c3a408eca786efaac1
christophboecker <christoph_boecker@t-online.de>
2018-07-18 21:24:21 +0200

    Umstellung auf 'includeCurrentPageSubPath'

--- pages/index.php
+++ pages/index.php
@@ -8,5 +8,5 @@
 echo rex_view::title($this->i18n('name'));
 
 if ($subpage = rex_be_controller::getCurrentPagePart(2)) {
-    include rex_be_controller::getCurrentPageObject()->getSubPath();
+    rex_be_controller::includeCurrentPageSubPath();
 }
