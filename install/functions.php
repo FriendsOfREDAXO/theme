673c93aa275908a93d32c7877b883d9762db42b4
Daniel Weitenauer <3332367+DanielWeitenauer@users.noreply.github.com>
2021-03-02 11:57:28 +0100

    Comment out all example code

--- install/functions.php
+++ install/functions.php
@@ -1,10 +1,11 @@
 <?php
+/*
 rex_extension::register('PACKAGES_INCLUDED', function () {
     // Register custom YRewrite scheme
-    /*if (rex_addon::get('yrewrite')->isAvailable()) {
+    if (rex_addon::get('yrewrite')->isAvailable()) {
         rex_yrewrite::setScheme(new my_rewrite_scheme());
-    }*/
+    }
 }, rex_extension::LATE);
-
+*/
 
 
