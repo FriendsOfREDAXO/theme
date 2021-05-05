89dc77445bf0402a8a6271f8162d5b19c6683ad3
Daniel Weitenauer <github@studio-ahoi.de>
2021-05-05 11:56:52 +0200

    Rename only if Developer >= 3.6.0 is available

--- update.php
+++ update.php
@@ -13,6 +13,7 @@ theme_sync_config();
  * Rename folder yform_emails to yform_email to ensure compatibility with developer addon
  * @version 1.3.0
  */
-if (file_exists(theme_path::base('private/redaxo/yform_emails'))) {
+if (rex_string::versionCompare(rex_addon::get('developer')->getVersion(), '3.6.0', '>=') &&
+    file_exists(theme_path::base('private/redaxo/yform_emails'))) {
     rename(theme_path::base('private/redaxo/yform_emails'), theme_path::base('private/redaxo/yform_email'));
 }
